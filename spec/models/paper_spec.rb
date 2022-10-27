d772a026fc9112bf226e72bb6b073497f5cd02ee
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2022-10-28 00:41:30 +0200

    Failing test for #12

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -11,5 +11,14 @@ describe "Paper model", type: :model do
   it "should not validate paper without a title" do
     paper = Paper.new(title: nil, venue: "test venue", year: 2000)
     expect(paper).to_not be_valid
+
+    paper = Paper.new(title: "test title", venue: nil, year: 2000)
+    expect(paper).to_not be_valid
+
+    paper = Paper.new(title: "test title", venue: "test value", year: nil)
+    expect(paper).to_not be_valid
+
+    paper = Paper.new(title: "test title", venue: "test venue", year: "2000")
+    expect(paper).to_not be_valid
   end
 end
