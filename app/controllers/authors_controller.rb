419c5afae0f64f9f80a94f79b71d27d524d0fe1e
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2022-10-27 20:12:27 +0200

    Fixed issue #9

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,10 @@ class AuthorsController < ApplicationController
         @author = Author.new
     end
 
+    def edit
+        @author = Author.find(params[:id])
+    end
+
     def create
         @author = Author.new(author_params)
 
@@ -10,6 +14,16 @@ class AuthorsController < ApplicationController
             redirect_to @author
         else
             render 'new'
+        end
+    end
+
+    def update
+        @author = Author.find(params[:id])
+
+        if @author.update(author_params)
+            redirect_to @author
+        else
+            render 'edit'
         end
     end
 
