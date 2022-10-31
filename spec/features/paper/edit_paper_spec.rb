e4666928701d9c9a075f17f172cc3f1fe6134b5a
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2022-11-01 00:28:42 +0100

    Fixed issue #17

---
+++ spec/features/paper/edit_paper_spec.rb
@@ -0,0 +1,11 @@
+#require 'rails_helper'
+
+#describe "Edit paper view", type: :feature do
+#  it "should allow author selection" do
+#    @alan = FactoryBot.create :author
+#    @paper = FactoryBot.create :paper
+#    visit paper_path(@paper)
+
+#    expect(page).to have_css("select[multiple]")
+#  end
+#end
